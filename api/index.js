cf931eaf3c4e55f4fc9eda2b63c9734fce431a49
andrew <andrew.ramell@gmail.com>
2023-05-03 06:51:15 -0400

    tst

--- api/index.js
+++ api/index.js
@@ -1,15 +1,6 @@
-const cron = require("node-cron");
+const app = require("express")();
+const { v4 } = require("uuid");
 
-const express = require("express");
-const axios = require("axios");
-const nodemailer = require("nodemailer");
-const { getLogger } = require("nodemailer/lib/shared");
-const PORT = process.env.PORT || 3001;
-
-require("dotenv").config();
-
-const app = express();
-const locationId = 8120;
 app.get("/api", (req, res) => {
   const path = `/api/item/${v4()}`;
   res.setHeader("Content-Type", "text/html");
@@ -22,64 +13,4 @@ app.get("/api/item/:slug", (req, res) => {
   res.end(`Item: ${slug}`);
 });
 
-// https://ttp.cbp.dhs.gov/schedulerapi/slot-availability?locationId=8120
-
-app.get("/", (req, res) => res.send("Main page"));
-
-app.get("/send-email", async (req, res) => {
-  try {
-    // Make the HTTP request
-    const response = await axios.get(
-      `https://ttp.cbp.dhs.gov/schedulerapi/slot-availability?locationId=${locationId}`
-    );
-
-    console.log(response.data);
-    // Send the email
-
-    if (response.data.availableSlots.length > 0) {
-      console.log("Slots available");
-      const transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-          user: process.env.EMAIL_ADDRESS,
-          pass: process.env.EMAIL_PASSWORD,
-        },
-      });
-
-      const mailOptions = {
-        from: "aramell7788@gmail.com",
-        to: "andrew.ramell@gmail.com",
-        subject: "Global Entry appointment available!",
-        text: `Next available appointment: ${response.data.availableSlots[0].startTimestamp} rawData: ${response.data.availableSlots}
-        
-        sign up here: https://ttp.cbp.dhs.gov/schedulerui/schedule-interview/location?lang=en&vo=true&returnUrl=ttp-external&service=UP
-        `,
-      };
-
-      await transporter.sendMail(mailOptions);
-
-      res.send("Email sent successfully");
-    } else {
-      console.log("No slots available");
-      const transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-          user: process.env.EMAIL_ADDRESS,
-          pass: process.env.EMAIL_PASSWORD,
-        },
-      });
-
-      const mailOptions = {
-        from: "aramell7788@gmail.com",
-        to: "andrew.ramell@gmail.com",
-        subject: "Global Entry not available",
-      };
-      await transporter.sendMail(mailOptions);
-    }
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Error sending email");
-  }
-});
-
-app.listen(PORT);
+module.exports = app;
