205b748793a0cc3d8258d00c2bd35bbea6b4851c
andrew <andrew.ramell@gmail.com>
2023-05-02 21:07:29 -0400

    adding send email cron

--- api/cron.js
+++ api/cron.js
@@ -1,9 +1,18 @@
-cron.schedule("*/1 * * * *", async () => {
+// cron.schedule("*/1 * * * *", async () => {
+//   try {
+//     // Call the rout
+//     const response = await axios.get("http://localhost:3000/send-email");
+//     console.log(response.data);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// });
+
+async function sendEmail() {
   try {
-    // Call the rout
     const response = await axios.get("http://localhost:3000/send-email");
     console.log(response.data);
   } catch (error) {
     console.error(error);
   }
-});
+}
